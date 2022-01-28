f0e6a0207870f565c9b9f436a569b6f2d06415e2
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2022-01-28 11:56:16 -0300

    feat: `pokemon-card` dinamicaly loads

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Pokemon } from 'src/_model/pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
+  @Input()
+  public pokemon: Pokemon = {} as Pokemon;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public padNumber(number: number): string {
+    const numberStr = number.toString();
+    return numberStr.padStart(3, '0');
   }
-
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/pokemon';
+import { Type } from 'src/_model/type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  styleUrls: ['./pokemon-list.component.sass'],
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [Type.Grass, Type.Poison],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [Type.Grass, Type.Poison],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
+      number: 6,
+      name: 'Charizard',
+      types: [Type.Fire, Type.Flying],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png',
+      number: 9,
+      name: 'Blastoise',
+      types: [Type.Water],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/050.png',
+      number: 50,
+      name: 'Diglett',
+      types: [Type.Ground],
+    },
+    {
+      image:
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png',
+      number: 54,
+      name: 'Psyduck',
+      types: [Type.Water],
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
+
+
+
